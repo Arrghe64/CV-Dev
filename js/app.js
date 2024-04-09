982261ef0822d0e4a2bd16397201215476b39a2e
Viviane <schommersv06@gmail.com>
2024-04-09 10:17:28 +0200

    ajout de boutons vers le top et update du css

--- js/app.js
+++ js/app.js
@@ -9,9 +9,18 @@ let toggle = document.querySelector('.toggle');
 let body = document.querySelector('body');
 let items = document.querySelector('.item');
 let section = document.querySelector("section");
+let buttonTop = document.querySelector(".btnBox");
 
 toggle.addEventListener('click', function () {
   body.classList.toggle('open');
+});
+
+buttonTop.addEventListener("click", function (event) {
+  event.preventDefault(); // Empêche le comportement par défaut du lien
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
 });
 
 //* envoi email
--- js/script.js
+++ js/script.js
@@ -1,7 +1,26 @@
 $(function(){
-    $(".navbar a, footer a").on("click", function(event){
-        event.preventDefault();
-        var hash = this.hash;
-        $('body,html').animate({scrollTop:$(hash).offset().top}, 900, function(){window.location.hash = hash;})
-    });
+  // Gestion du clic sur les liens de la navbar, du footer
+  $(".navbar a, footer a").on("click", function (event) {
+    event.preventDefault();
+    var hash = this.hash;
+
+    // Animation du scroll vers la section correspondante
+    $("body,html").animate(
+      { scrollTop: $(hash).offset().top },
+      900,
+      function () {
+        window.location.hash = hash;
+      }
+    );
+  });
+
+  // Gestion du clic sur les boutons avec la classe .button2
+  $(".button2, .button1").on("click", function (event) {
+    event.preventDefault();
+    var target = $(this).attr("href");
+
+    // Animation du scroll vers la section correspondante
+    $("html, body").animate({ scrollTop: $(target).offset().top }, "slow");
+  });
 })
+    
